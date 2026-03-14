7af557c938dc65574486cb05cc6391f01a40db5b
Yailín <tu-correo@github.com>
2026-03-14 10:08:33 -0500

    Initial React dashboard project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Climate Data Analytics Dashboard</h1>
+      <p>Visualizing climate data using APIs and charts.</p>
+    </div>
+  )
+}
+
+export default App
